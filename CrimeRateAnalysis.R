c959cdfa0644800555a7a7c47203f8e83de8ef44
RLurtenManchester <86394471+RLurtenManchester@users.noreply.github.com>
2021-07-18 23:41:44 -0500

    Update CrimeRateAnalysis.R

--- CrimeRateAnalysis.R
+++ CrimeRateAnalysis.R
@@ -1,6 +1,8 @@
 ## Import Dataset
 CrimeRate2000thru2019A <- CrimeRate2000thru2019[, 1:20]
 
+#Load ggplot
+library(ggplot2)
 # Narrowing down the Data
 ## Lets Subset this
 keeps <- c("Year", "ViolentCrimeRate", "Murder/NonNegligentManslaughterRate", "Rape(LegacyDefinition)Rate", "RobberyRate", "AggravatedAssaultRate")
@@ -8,3 +10,15 @@ CrimeRate2000thru2019Rate <- CrimeRate2000thru2019A[keeps]
 
 CrimeRate2000thru2019B <- as.matrix(CrimeRate2000thru2019Rate)
 heatmap(CrimeRate2000thru2019B)
+
+barchart(CrimeRate2000thru2019Rate$ViolentCrimeRate)
+# Converting everything into factors
+
+Crime <- c(1:6, )
+CrimeRate2000thru2019Rate[,Crime] <- lapply(CrimeRate2000thru2019Rate[,Crime] , factor)
+str(CrimeRate2000thru2019Rate)
+
+CrimeRate2000thru2019RateA <- as.factor(CrimeRate2000thru2019Rate)
+
+ggplot(CrimeRate2000thru2019A, aes(CrimeRate2000thru2019A$`Murder/NonNegligentManslaughter`)) + geom_bar()
+### This isnt seeming to do what i want and Tableau created the graphs I wanted
